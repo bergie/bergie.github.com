9556bcabeb9b32322c7071c35349a525808e75fd
Henri Bergius <henri.bergius@iki.fi>
2011-10-31 23:55:55 +0200

    Blog to JSON-LD

---
+++ blogdump.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Dump Midgard1 articles to JSON-LD format
+ */
+$mgd = new midgard_connection();
+$mgd->open('midgard');
+$mgd->set_sitegroup('bergie.iki.fi');
+
+$qb = new midgard_query_builder('midgard_article');
+$qb->add_constraint('topic.component', '=', 'net.nehmer.blog');
+$qb->add_constraint('topic.name', '=', 'blog');
+$qb->add_order('metadata.published', 'ASC');
+$articles = $qb->execute();
+
+include('/usr/share/php/midcom/lib/net/nehmer/markdown/lib/markdown.php');
+
+$ontologies = array
+(
+    'dc' => 'http://purl.org/dc/elements/1.1/',
+    'atom' => 'http://www.w3.org/2005/Atom',
+);
+
+$linkeddata = array();
+$identifier_base = 'http://bergie.iki.fi/blog/';
+foreach ($articles as $article)
+{
+    $identifier = "{$identifier_base}{$article->name}/";
+
+    $schema = $article->get_parameter('midcom.helper.datamanager2', 'schema_name');
+    switch ($schema)
+    {
+        case 'html':
+            $content = $article->content;
+            break;
+        case 'markdown':
+        default:
+            $content = Markdown($article->content);
+            break;
+    }
+
+    $linkeddata_entry = array
+    (
+        '#' => $ontologies,
+        '@' => "<{$identifier}>",
+        'a' => 'atom:entry',
+        'dc:title' => $article->title,
+        'dc:creator' => '<http://bergie.iki.fi/#me>',
+        'dc:date' => $article->metadata->published,
+        'atom:content' => $content,
+    );
+    //die(json_encode($linkeddata_entry));
+    $linkeddata[] = $linkeddata_entry;
+}
+echo json_encode($linkeddata);
+?>
